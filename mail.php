5ec7b1c5e0d31b4f20d126a2de398d0ec3d760f3
Alex Savi <alex.savi.kv@gmail.com>
2023-02-20 21:56:28 +0530

    add email

--- mail.php
+++ mail.php
@@ -15,7 +15,7 @@ $message .= "\n\n\nName:" . $name;
 $message .= "\nEmail:" . $email;
 $message .= "\nPhone:" . $phone;
 $message .= "\nMessage Content:" . $content;
-$header = "From: info@radspanwireless.com\r\n";
+$header = "From: alex.savi.kv@gmail.com\r\n";
 // $header = "Cc: \r\n";
 // $header = "BCc:\r\n";
 $header .= "MIME-Version: 1.0\r\n";
@@ -23,10 +23,8 @@ $header .= "Content-type: text/html\r\n";
 //send email
 $retval = mail($to, $subject, $message);
 if ($retval == 1) {
-    alert("successs");
     header("Location:index.html");
 } else {
-    alert("failed");
     echo "Something went wrong! Mail senting failed........";
 }
 //Email response
